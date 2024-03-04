79133ba4d79cdac6e5b47cdfe6076799579c83db
kidchang93 <lck930721@gmail.com>
2024-03-04 16:40:12 +0900

    라우터 사용해서 SPA 적용하기

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import Profile from "./pages/Profile";
 import Articles from "./pages/Articles";
 import Article from "./pages/Article";
 import Layout from "./pages/Layout";
+import NotFound from "./pages/NotFound";
+import Login from "./pages/Login";
+import MyPage from "./pages/MyPage";
 
 const App = () => {
 	return (
@@ -18,6 +21,10 @@ const App = () => {
       <Route path="/articles" element={<Articles />}>
         <Route path=":id" element={<Article />} />
       </Route>
+      <Route path="/login" element={<Login />} />
+      <Route path="/mypage" element={<MyPage/>} />
+      <Route path="*" element={<NotFound />} />
+
 		</Routes>
 	);
 };
