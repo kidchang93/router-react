4551f5f1a7b988e1cc326b74d191025f8475b563
kidchang93 <lck930721@gmail.com>
2024-03-04 16:27:55 +0900

    404페이지 만들기

--- src/pages/Articles.js
+++ src/pages/Articles.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import { Link, Outlet } from 'react-router-dom';
+import { Link, NavLink, Outlet } from 'react-router-dom';
 
 const Articles = () => {
+  const activeStyle = { color: 'green', fontSize: 21 };
   return (
     <div>
-      <Outlet/>
-      <Link to='/articles/1'>게시글1</Link> |
+      <Outlet />
+      <NavLink to='/articles/1' style={({isActive}) => (isActive ? activeStyle : undefined)}>게시글 1</NavLink>
+      <NavLink to='/articles/2' style={({ isActive }) => (isActive ? activeStyle : undefined)}>게시글 2</NavLink>
+      <NavLink to='/articles/3' style={({isActive}) => (isActive ? activeStyle : undefined)}>게시글 3</NavLink>
+      {/* <Link to='/articles/1'>게시글1</Link> |
       <Link to='/articles/2'>게시글2</Link> |
-      <Link to='/articles/3'>게시글3</Link>
+      <Link to='/articles/3'>게시글3</Link> */}
     </div>
   );
 };
